45f7df71d13deca54532338918b97bd1f4abc6da
fullalive <sixaxis1996@gmail.com>
2018-08-10 17:12:47 +0500

    * a little bit redesign start screen

--- src/AppProvider.js
+++ src/AppProvider.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import App from './App';
 import Game from './screens/Game';
+import Main from './screens/Main';
 import EndGame from './screens/EndGame';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import createBrowserHistory from 'history/createBrowserHistory';
@@ -19,7 +20,7 @@ export default class AppProvider extends Component {
 				<Switch>
 					<Route path='/game'><Game setScores={this.setScores} history={history} /></Route>
 					<Route path='/end' ><EndGame scores={this.state.scores} /></Route>
-					<Route path='/' ><App /></Route>
+					<Route path='/' ><Main /></Route>
 				</Switch>
 			</Router>
 		);
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,58 @@
 import React, {Component} from 'react';
 import {Flex} from 'grid-styled';
 import styled from 'styled-components';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 export default class App extends Component {
+	state = {
+		height: 1000,
+	};
+
+	componentDidMount() {
+		const height = document.documentElement.clientHeight;
+		this.setState({height});
+	}
+
 	render() {
+		const {height} = this.state;
 		return (
-			<button onClick={() => window.history.pushState('', '', '/game')}>Начать игру</button>
+			<Main height={height}>
+				<LogoImg src={require('../images/logo.png')} alt='logo' />
+				<StartButton><Link className='link' to='/game'>Начать игру</Link></StartButton>
+			</Main>
 		);
 	}
 }
+
+const Main = styled(({height, ...props}) => <Flex {...props} />)`
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: ${({height}) => `${height}px`};
+	background-color: rgb(0, 57, 22);
+`;
+
+const StartButton = styled.button`
+	border-radius: 3px;
+	border-width: 0;
+	background-color: rgba(255, 252, 252, 0.9);
+	height: 35px;
+	width: 130px;
+	
+	.link {
+		display: inline-block;
+		color: rgb(23, 119, 14);
+		letter-spacing: 1px;
+		font-weight: bold;
+		padding: 10px 13px;
+		text-decoration: none;
+		:hover {
+		    transform: scale(1.1);
+		    transition: 0.5s;
+		}
+	}
+`;
+
+const LogoImg = styled.img`
+	height: 500px;
+`;
